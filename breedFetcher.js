f5c1e4ef440257869906f669441a12ed5c26b4b1
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-07-07 19:53:53 +0000

    refractor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,22 @@
 const request = require('request');
-const name = process.argv.slice(2);
-request("https://api.thecatapi.com/v1/breeds", (error, response, body) => {
-  const data = JSON.parse(body);
+
+const fetchBreedDescription = function(breedName, callback) {
+  let err = null;
   let description = "";
-  for (const breed of data) {
-    if (breed.name === name[0]) {
-      description = breed.description;
-      console.log(description);
+  request("https://api.thecatapi.com/v1/breeds", (error, response, body) => {
+    const data = JSON.parse(body);
+    for (const breed of data) {
+      if (breed.name === breedName) {
+        description = breed.description;
+      }
     }
-  }
-  if (!description) console.log("breed not found");
-  if (error) console.log(error);
-});
+    if (!description) description = "breed not found";
+    if (error) err = error;
+    callback(err, description);
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
 
 
